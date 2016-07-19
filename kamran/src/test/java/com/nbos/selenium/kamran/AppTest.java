f38aff412c4c2058dd7f7a654eb45a179385406a
Kamran p <kamranp@nbos.vinisoft.com>
2016-07-19 18:21:02 +0530

    added a new testing project

---
+++ kamran/src/test/java/com/nbos/selenium/kamran/AppTest.java
@@ -0,0 +1,15 @@
+package com.nbos.selenium.kamran;
+
+import org.testng.annotations.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	@Test
+	public void test(){
+		System.out.println("1231312");
+	}
+}
